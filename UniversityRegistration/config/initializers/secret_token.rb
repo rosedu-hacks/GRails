6337f966210361a03e5305f8e70c53cc235b39c5
Ada <ada@localhost.localdomain>
2013-10-19 16:51:21 +0300

    Created app

---
+++ UniversityRegistration/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UniversityRegistration::Application.config.secret_key_base = '58ece86e8774f751df8ac301178196f5a988ce2527d804888c377d3f334f25d6769c92808749ee050ad78c8fcc53aaf08010b7b67a3be7e880e10741fbce94f8'
---
+++ UniversityRegistration/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UniversityRegistration::Application.config.session_store :cookie_store, key: '_UniversityRegistration_session'
