5cd9f08ada3791b6a30b099368576b0c5da4c02d
marianitadn <maria.nita.dn@gmail.com>
2013-10-19 21:08:01 +0300

    Glue to users, validate student field #10

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,9 @@
 class Student < ActiveRecord::Base
+    validates  :father_name, presence: true, length: { maximum: 50 }
+    validates :mother_name, presence: true, length: { maximum: 50 }
+    validates :status, presence: true
+    validates_inclusion_of :status, :in => [['New', 'new'], ['Accepted', 'accepted'],
+        ['Enrolled', 'enrolled'], ['Alumni', 'alumni'], ['Rejected', 'rejected']]
+
+    belongs_to :user
 end
