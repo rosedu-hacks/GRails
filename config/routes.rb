6369b6db787cd0ac33646358e063f58576daca42
marianitadn <maria.nita.dn@gmail.com>
2013-10-19 20:37:48 +0300

    Update routes #3

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 UniversityRegistration::Application.routes.draw do
-  resources :students
-
   # Static pages
-  root to: 'static_pages#home'
+  root 'static_pages#home'
   get "static_pages/about"
   # Users pages
   devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  resources :students
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
