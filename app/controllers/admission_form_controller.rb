184ad5df7455a8b6fb582aabebe28febd891e33b
marianitadn <maria.nita.dn@gmail.com>
2013-10-19 20:39:33 +0300

    Add admission form controller #3

---
+++ app/controllers/admission_form_controller.rb
@@ -0,0 +1,2 @@
+class AdmissionFormController < ApplicationController
+end
---
+++ test/controllers/admission_form_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AdmissionFormControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
