1d5403fc569e3fc6d811c1d2dc63fb621f6f5df2
marianitadn <maria.nita.dn@gmail.com>
2013-10-20 11:12:18 +0300

    Put FB attributes before sign in

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,15 +1,14 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+    @user.last_name = request.env["omniauth.auth"][:extra][:raw_info][:last_name]
+    @user.first_name = request.env["omniauth.auth"][:extra][:raw_info][:first_name]
+    @user.url = request.env["omniauth.auth"][:extra][:raw_info][:link]
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      current_user.last_name = session["devise.facebook_data"][:extra][:raw_info][:last_name]
-      current_user.first_name = session["devise.facebook_data"][:extra][:raw_info][:first_name]
-      current_user.url = session["devise.facebook_data"][:extra][:raw_info][:link]
-      current_user.save
       redirect_to root_path
     end
   end
