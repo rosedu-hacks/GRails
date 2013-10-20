3a43ee621968a06c10512d5fdb2e6d3f4072a7db
marianitadn <maria.nita.dn@gmail.com>
2013-10-20 10:05:06 +0300

    Add connect button

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ UniversityRegistration::Application.routes.draw do
   match :about, to: "static_pages#about", via: :get
   match :user_type, to: "static_pages#user_type", via: :get
   match :profile, to: "students#show", via: :get
+  match :connect, to: "students#show_connections", via: :get
   match 'admission_forms/new', to: 'admission_forms#new', :via => :get, action: 'new'
   match 'admission_forms/populate', to: 'admission_forms#populate', :via => :post, action: 'populate'
   # Users pages
